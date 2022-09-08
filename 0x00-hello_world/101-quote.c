25531b9f57891c7ffee7353d5706640269faa914
sheynex <afolabishenex@gmail.com>
2022-09-08 15:35:50 -0700

    Adding 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,9 +7,8 @@
  * Return: 1
 */
 int main(void)
-{
-	int s=strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+{	
+	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
 	
 	return (1);
 }
