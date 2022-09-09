05f0d07715aca807e0aaecf6c93c67a3703c78c8
Afolabi Oluwaseun Joseph <46227199+sheynex@users.noreply.github.com>
2022-09-09 22:24:49 +0100

    Task 5 print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ *
+ * Description: Print all digits of base 10 starting from 0
+ *
+ * Return: 0
+ *
+ */
+
+
+int main(void)
+
+{
+
+	int i;
+
+	i = 0;
+
+	while (i < 10)
+	{
+
+		printf("%i", i);
+
+		i++;
+
+	}
+
+	printf("\n");
+
+
+	return (0);
+
+}
