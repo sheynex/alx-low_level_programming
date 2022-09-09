238398a68ed69901f8a57735e3c595666c21a038
sheynex <afolabishenex@gmail.com>
2022-09-09 13:07:07 -0700

    Solution to 4-print_alphabet.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main - main block
+ *
+ * Description: Print the alphabet in lowercase except q and e
+ *
+ * Return: 0
+ *
+ */
+int main(void)
+
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= "z"; alphabet++)
+	{
+		if (alphabet != 'e' && alphabet != 'q')
+			putchar(alphabet);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
+
+
+
+
+
+
+}
