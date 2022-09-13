2d55f1d37450b30d8c9ab5e5421ffe43ade99cf7
sheynex <afolabishenex@gmail.com>
2022-09-13 16:12:47 -0700

    Task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,9 +12,9 @@ void jack_bauer(void)
 	int hours_remainder;
 	int mins_remainder;
 
-	while (hours <= 23)	
+	while (hours <= 23)
 	{
-		while (minutes <= 59)
+	while (minutes <= 59)
 	{
 	mins_remainder = minutes % 10;
 	hours_remainder = hours % 10;
