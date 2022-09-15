191058ebce160292d061fc6bdcc31a3c0d3e342b
sheynex <afolabishenex@gmail.com>
2022-09-15 04:10:06 -0700

    2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -15,7 +15,7 @@ if (a >= b && b >= c)
 {
 largest = a;
 }
-else if (b >= a && a >= c)
+else if (b >= a && b >= c)
 {
 largest = b;
 }
@@ -23,6 +23,5 @@ else
 {
 largest = c;
 }
-
 return (largest);
 }
