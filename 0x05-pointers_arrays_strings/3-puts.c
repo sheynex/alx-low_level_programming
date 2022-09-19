21325bad3dd49c373df1af7e155334e1baa5dab8
sheynex <afolabishenex@gmail.com>
2022-09-19 07:42:38 -0700

    3-puts

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -3,10 +3,10 @@
 
 /**
  * _puts - print a string
- * @strt: string to print
+ * @str: string to print
  *
- * Descriptio - print a string on success
- * returns no error
+ * Description: prints a string 
+ * On success: returns no error
  */
 
 void _puts(char *str)
