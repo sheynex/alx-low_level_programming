106b17e68505458ccabe0c243a98ddc0338a08b9
sheynex <afolabishenex@gmail.com>
2022-09-19 12:43:56 -0700

    Task 100

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,26 +9,20 @@
 
 int _atoi(char *s)
 {
-	int sign;
-	unsigned int num;
-	char *temp;
+	int sign = 1;
+	unsigned int num = 0;
 
-	temp = s;
-	num = 0;
-	sign = 1;
-	while (*temp != '\0' && (*temp < '0' || *temp > '9'))
+	do
 	{
-		if (*temp == '-')
+		if (*s == '-')
 			sign *= -1;
-		temp++;
-	}
-	if (*temp != '\0')
-	{
-		do {
-			num = num * 10 + (*temp - '0');
-			temp++;
-		} while (*temp >= '0' && *temp <== '9');
-	}
-	return (num * sign);
 
+		else if (*s >= '0' && *s <= '9')
+			num = (num * 10) + (*s - '0');
+
+		else if (num > 0)
+			break;
+	} while (*s++);
+
+	return (num * sign);
 }
