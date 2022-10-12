1ac692c8bcc07ffe21740d5445a5e64104fb70fe
sheynex <afolabishenex@gmail.com>
2022-10-12 14:03:50 -0700

    task update;

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,16 +1,16 @@
-#include <stdlib.h>
 #include "function_pointers.h"
 
 /**
  * print_name - prints a name
  * @name: name to print
  * @f: pointer to the printing fucntion
+ *
+ * Retuen: void
  */
 
 void print_name(char *name, void (*f)(char *))
 
 {
-	if (!name || !f)
-		return;
+	if (name != NULL && f!=NULL)
 	f(name);
 }
